28d633c6b80477a671a60cd93d628f68e6c6ff57
CDSturgeon <CDSturgeon@protonmail.com>
2022-01-27 15:34:33 -0500

    Alphabet methods in their own file

---
+++ Alphabet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace CodeLouisvilleDemo;
+
+public class Alphabet
+{
+    public static string PrintAlphabet()
+    {
+        StringBuilder alphabet = new();
+        for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+        {
+            alphabet.Append(alpha);
+        }
+        return alphabet.ToString();
+    }
+
+    public static string PrintAlphabetSkipOneLetter()
+    {
+        StringBuilder alphabet = new();
+        for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + 2))//cast (alpha + 2) as type char
+        {
+            alphabet.Append(alpha);
+        }
+        return alphabet.ToString();
+    }
+
+    public static string PrintAlphabetBackwards()
+    {
+        StringBuilder alphabet = new();
+        for (char alpha = 'Z'; alpha >= 'A'; alpha--)
+        {
+            alphabet.Append(alpha);
+        }
+        return alphabet.ToString();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,17 @@
 ﻿using System;
 using System.Text;
 
-namespace CodeLouisvilleDemo
+namespace CodeLouisvilleDemo;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Alphabet in order\n" + PrintAlphabet() + Environment.NewLine);
-            Console.WriteLine("Every other letter\n" + PrintAlphabetSkipOneLetter() + Environment.NewLine);
-            Console.WriteLine("Alphabet in reverse\n" + PrintAlphabetBackwards() + Environment.NewLine);
-        }
 
-        public static string PrintAlphabet()
-        {
-            StringBuilder alphabet = new StringBuilder();
-            for (char alpha = 'A'; alpha <= 'Z'; alpha++)
-            {
-                alphabet.Append(alpha);
-            }
-            return alphabet.ToString();
-        }
+        Console.WriteLine("Alphabet in order\n" + Alphabet.PrintAlphabet() + Environment.NewLine);
 
-        public static string PrintAlphabetSkipOneLetter()
-        {
-            StringBuilder alphabet = new StringBuilder();
-            for (char alpha = 'A'; alpha <= 'Z'; alpha = (char) (alpha + 2))
-            {
-                alphabet.Append(alpha);
-            }
-            return alphabet.ToString();
-        }
+        Console.WriteLine("Every other letter\n" + Alphabet.PrintAlphabetSkipOneLetter() + Environment.NewLine);
 
-        public static string PrintAlphabetBackwards()
-        {
-            StringBuilder alphabet = new StringBuilder();
-            for (char alpha = 'Z'; alpha >= 'A'; alpha--)
-            {
-                alphabet.Append(alpha);
-            }
-            return alphabet.ToString();
-        }
+        Console.WriteLine("Alphabet in reverse\n" + Alphabet.PrintAlphabetBackwards() + Environment.NewLine);
     }
 }
