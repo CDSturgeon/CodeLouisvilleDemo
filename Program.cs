346583b4ab71cdaefc0918504cb85d624a9568a8
CDSturgeon <CDSturgeon@protonmail.com>
2022-01-27 22:49:37 -0500

    added menu

--- Program.cs
+++ Program.cs
@@ -7,11 +7,48 @@ class Program
 {
     static void Main(string[] args)
     {
+        Menu();
+        Console.WriteLine("\n\nGoodbye.");
+    }
 
-        Console.WriteLine("Alphabet in order\n" + Alphabet.PrintAlphabet() + Environment.NewLine);
+    static void Menu()
+    {
+        char input = new();
+        while (input != 'Q' || input != 'q')
+        {
+            Console.Clear();
+            Console.WriteLine("How do you want to print the alphabet?\n" +
+                              "F: Forward\n" +
+                              "B: Backward\n" +
+                              "Q: Quit");
 
-        Console.WriteLine("Every other letter\n" + Alphabet.PrintAlphabetSkipOneLetter() + Environment.NewLine);
+            input = Console.ReadKey().KeyChar;
 
-        Console.WriteLine("Alphabet in reverse\n" + Alphabet.PrintAlphabetBackwards() + Environment.NewLine);
+            switch (input) //Switch on Key enum
+            {
+                case 'F' or 'f':
+                    Console.WriteLine("\n\nAlphabet in order\n" + Alphabet.PrintAlphabet() + Environment.NewLine);
+                    Wait();
+                    break;
+                case 'B' or 'b':
+                    Console.WriteLine("\n\nAlphabet in reverse\n" + Alphabet.PrintAlphabetBackwards() + Environment.NewLine);
+                    Wait();
+                    break;
+                case 'Q' or 'q':
+                    return;
+                default:
+                    Console.WriteLine($"\n\nSelection '{input}' not recognized. Try again.\n");
+                    Wait();
+                    break;
+            }
+        }
+        //Console.WriteLine("Every other letter\n" + Alphabet.PrintAlphabetSkipOneLetter() + Environment.NewLine);
+    }
+
+    static void Wait()
+    {
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+        return;
     }
 }
