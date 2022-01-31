f4f8b672fc6762909c998f6122e0e8d6f9d745c9
CDSturgeon <CDSturgeon@protonmail.com>
2022-01-31 12:39:47 -0500

    Menu updates. Added EveryXLetter()

--- Alphabet.cs
+++ Alphabet.cs
@@ -5,20 +5,54 @@ namespace CodeLouisvilleDemo;
 
 public class Alphabet
 {
-    public static string PrintAlphabet()
+    public static void Menu()
     {
-        StringBuilder alphabet = new();
-        for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+        char input = new();
+        Console.Clear();
+        Console.WriteLine("How do you want to print the alphabet?\n" +
+                              "F: Forward\n" +
+                              "B: Backward\n" +
+                              "Q: Quit");
+
+        input = Console.ReadKey().KeyChar;
+        switch (input) //Switch on Key enum
         {
-            alphabet.Append(alpha);
+            case 'F' or 'f':
+                Console.WriteLine("\nAlphabet in order\n" + Alphabet.PrintAlphabet() + Environment.NewLine);
+                Wait("continue");
+                break;
+            case 'B' or 'b':
+                Console.WriteLine("\nAlphabet in reverse\n" + Alphabet.PrintAlphabetBackwards() + Environment.NewLine);
+                Wait("continue");
+                break;
+            case 'Q' or 'q':
+                return;
+            default:
+                Console.WriteLine($"\nSelection '{input}' not recognized.\n");
+                Wait("try again");
+                break;
         }
-        return alphabet.ToString();
     }
 
-    public static string PrintAlphabetSkipOneLetter()
+    private static void Wait(string text)
     {
+        Console.WriteLine($"Press the spacebar to {text}.");
+        bool space = false;
+        while (space == false)
+        {
+            if (Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+                space = true;
+        }
+        return;
+    }
+
+    private static string PrintAlphabet()
+    {
+        int x = EveryXLetter();
+
         StringBuilder alphabet = new();
-        for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + 2))//cast (alpha + 2) as type char
+
+        for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + x))
         {
             alphabet.Append(alpha);
         }
@@ -27,11 +61,50 @@ public class Alphabet
 
     public static string PrintAlphabetBackwards()
     {
+        int x = EveryXLetter();
+
         StringBuilder alphabet = new();
-        for (char alpha = 'Z'; alpha >= 'A'; alpha--)
+
+        for (char alpha = 'Z'; alpha >= 'A'; alpha = (char)(alpha - x))
         {
             alphabet.Append(alpha);
         }
         return alphabet.ToString();
+    }
+
+    //public static string PrintAlphabetSkipOneLetter()
+    //{
+    //    StringBuilder alphabet = new();
+    //    for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + 2))//cast (alpha + 2) as type char
+    //    {
+    //        alphabet.Append(alpha);
+    //    }
+    //    return alphabet.ToString();
+    //}
+
+    public static int EveryXLetter()
+    {
+        Console.WriteLine("\n\nHow would you like to print the alphabet?\n" +
+                          "1: Print all letters\n" +
+                          "2: Print every other letter\n" +
+                          "or type any other number to print every 'x' letter\n");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int x))
+            {
+                if (x > 0)
+                    return x;
+                else
+                {
+                    Console.WriteLine("\nSelection must be greater than 0");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"\n{input} is not an integer.\n" +
+                                  "Please enter a whole number:");
+            }
+        }        
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,48 +7,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Menu();
+        Alphabet.Menu();
         Console.WriteLine("\n\nGoodbye.");
-    }
-
-    static void Menu()
-    {
-        char input = new();
-        while (input != 'Q' || input != 'q')
-        {
-            Console.Clear();
-            Console.WriteLine("How do you want to print the alphabet?\n" +
-                              "F: Forward\n" +
-                              "B: Backward\n" +
-                              "Q: Quit");
-
-            input = Console.ReadKey().KeyChar;
-
-            switch (input) //Switch on Key enum
-            {
-                case 'F' or 'f':
-                    Console.WriteLine("\n\nAlphabet in order\n" + Alphabet.PrintAlphabet() + Environment.NewLine);
-                    Wait();
-                    break;
-                case 'B' or 'b':
-                    Console.WriteLine("\n\nAlphabet in reverse\n" + Alphabet.PrintAlphabetBackwards() + Environment.NewLine);
-                    Wait();
-                    break;
-                case 'Q' or 'q':
-                    return;
-                default:
-                    Console.WriteLine($"\n\nSelection '{input}' not recognized. Try again.\n");
-                    Wait();
-                    break;
-            }
-        }
-        //Console.WriteLine("Every other letter\n" + Alphabet.PrintAlphabetSkipOneLetter() + Environment.NewLine);
-    }
-
-    static void Wait()
-    {
-        Console.WriteLine("Press any key to continue.");
-        Console.ReadKey();
-        return;
     }
 }
