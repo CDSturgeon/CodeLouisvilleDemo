6d9bc5d8163ec5124a30758acb3ce254abd0c07f
CDSturgeon <CDSturgeon@protonmail.com>
2022-02-02 16:38:21 -0500

    Updated methods and formatting of console output and input

--- Alphabet.cs
+++ Alphabet.cs
@@ -5,32 +5,43 @@ namespace CodeLouisvilleDemo;
 
 public class Alphabet
 {
+    //Menu loops until user quits program
     public static void Menu()
     {
         char input;
+        string cont = "continue";//string used more than once, variable created to reduce duplication
 
         while (true)
         {
             Console.Clear();
             Console.WriteLine("How do you want to print the alphabet?\n" +
-                                  "F: Forward\n" +
-                                  "B: Backward\n" +
-                                  "Q: Quit");
+                              "F: Forward\n" +
+                              "B: Backward\n" +
+                              "Q: Quit");
 
+            Console.Write("Selection: ");//User inputs shows on same line
             input = Console.ReadKey().KeyChar;
 
-            switch (input) //Switch on Key enum
+            switch (input)
             {
+                //Print alphabet forward
                 case 'F' or 'f':
-                    Console.WriteLine("\nAlphabet in order\n" + Alphabet.PrintAlphabet(Every_n_Letter()) + Environment.NewLine);
-                    Wait("continue");
+                    Console.WriteLine("\nAlphabet in order\n" +
+                                      Alphabet.BuildAlphabetA_Z(Every_n_Letter()) +
+                                      Environment.NewLine);
+                    Wait(cont);
                     break;
+                //Print alphabet backward
                 case 'B' or 'b':
-                    Console.WriteLine("\nAlphabet in reverse\n" + Alphabet.PrintAlphabetBackwards(Every_n_Letter()) + Environment.NewLine);
-                    Wait("continue");
+                    Console.WriteLine("\nAlphabet in reverse\n" +
+                                      Alphabet.BuildAlphabetZ_A(Every_n_Letter()) +
+                                      Environment.NewLine);
+                    Wait(cont);
                     break;
+                //Quit program
                 case 'Q' or 'q':
                     return;
+                //Invalid input
                 default:
                     Console.WriteLine($"\nSelection '{input}' not recognized.\n");
                     Wait("try again");
@@ -39,6 +50,9 @@ public class Alphabet
         }
     }
 
+    //Pause program, wait for user to press spacebar to continue
+    //
+    //Should the Console.WriteLine be removed? The instructions are specific to the function of this method.
     private static void Wait(string text)
     {
         Console.WriteLine($"Press the spacebar to {text}.");
@@ -53,7 +67,8 @@ public class Alphabet
         return;
     }
 
-    private static string PrintAlphabet(int n)
+    //Create the alphabet string forwards, showing every n letter
+    private static string BuildAlphabetA_Z(int n)
     {
         StringBuilder alphabet = new();
 
@@ -61,18 +76,31 @@ public class Alphabet
         {
             alphabet.Append(alpha);
         }
-        return alphabet.ToString();
+        return ShowEvery_n_LetterText(n) + alphabet.ToString();
     }
 
-    public static string PrintAlphabetBackwards(int n)
+    //Create the alphabet string backwards, showing every n letter
+    public static string BuildAlphabetZ_A(int n)
     {
         StringBuilder alphabet = new();
+        //string output;
 
         for (char alpha = 'Z'; alpha >= 'A'; alpha = (char)(alpha - n))
         {
             alphabet.Append(alpha);
         }
-        return alphabet.ToString();
+        return ShowEvery_n_LetterText(n) + alphabet.ToString();
+    }
+
+    //Returns more output text when skipping letters
+    public static string ShowEvery_n_LetterText(int n)
+    {
+        string clarify = "";
+        if (n > 1)
+        {
+            clarify = $"Showing every {n} letters\n";
+        }
+        return clarify;
     }
 
     //public static string PrintAlphabetSkipOneLetter()
@@ -85,12 +113,16 @@ public class Alphabet
     //    return alphabet.ToString();
     //}
 
+    //Collect input from user, parse string to int, error checking inputs
+    //
+    //Are there too many things happening in this method?
     public static int Every_n_Letter()
     {
         Console.WriteLine("\n\nHow would you like to print the alphabet?\n" +
                           "1: Print all letters\n" +
                           "2: Print every other letter\n" +
-                          "or type any other number to print every 'x' letter");
+                          "or type any other number to print every 'n' letter");
+        Console.Write("Enter a value for n: ");
 
         while (true)
         {
@@ -102,11 +134,12 @@ public class Alphabet
                 if (n > 0)
                     return n;
                 else
-                    Console.WriteLine("\nSelection must be greater than 0"); //loop continues
+                    Console.Write("\nSelection must be greater than 0\n" +
+                                  "Enter a number greater than 0: "); //loop continues
             }
             else
-                Console.WriteLine($"\n{input} is not an integer.\n" +
-                                  "Please enter a whole number:");
+                Console.Write($"\n{input} is not an integer.\n" +
+                                  "Please enter a whole number: ");
         }        
     }
 }
